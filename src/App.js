61d149171e60f819a87cf59b5af66f7c984d65f1
henryliu93 <88024437+henryliu93@users.noreply.github.com>
2021-09-09 00:21:09 +0800

    add template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="site-wrapper">
+
+      <div className="site-wrapper-inner">
+
+        <div className="cover-container">
+
+          <div className="masthead clearfix">
+            <div className="inner">
+              <h3 className="masthead-brand">Battle Loot</h3>
+              <nav>
+                <ul className="nav masthead-nav">
+                  <li className="active"><a href="#">Wallet Status</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+
+          <div className="inner cover">
+            <div className="row">
+              <div className="col-md-5">
+                Top Left
+                <a href="#" className="btn btn-lg btn-default">Learn more</a>
+              </div>
+              <div className="col-md-5 col-md-offset-2">Top Right</div>
+            </div>
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2">Central Panel</div>
+            </div>
+          </div>
+
+          <div className="mastfoot">
+            <div className="inner">
+              <p>Power By <a className="https://getbootstrap.com/">Battle Loot</a>, by <a href="https://twitter.com/mdo">@battle loot</a>.</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
